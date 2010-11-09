a6bd3794f16c609ef309d869d57972a82151b7f4
Mathijs de Bruin <drbob@dokterbob.net>
2010-11-09 15:18:31 +0100

    Start of basic_webshop app contents.

--- models.py
+++ models.py
@@ -1,8 +1,28 @@
-from webshop.core.models import ProductBase
+from webshop.core.models import ProductBase, CartBase, OrderBase
+
+from webshop.extensions.category.simple.models import CategoryBase, \
+                                                      CategorizedProductBase
 
 from django.db import models
 
-class Product(ProductBase):
-    """ Basic product model """
+
+class Product(CategorizedProductBase):
+    """ Basic product model. """
 
     pass
+
+class Cart(CartBase):
+    """ Basic shopping cart model. """
+    
+    pass
+
+class Order(OrderBase):
+    """ Basic order model. """
+    
+    pass
+
+class Category(CategoryBase):
+    """ Basic category model. """
+    
+    pass
+
---
+++ urls.py
@@ -0,0 +1,8 @@
+from surlex.dj import surl
+
+from django.conf.urls.defaults import *
+
+
+urlpatterns = patterns('basic_webshop.views',
+    # Do stuff
+)
