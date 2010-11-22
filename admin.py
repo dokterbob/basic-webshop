3c74bdb88c29140c265caeff572e003ac54c93bb
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-11-22 17:22:54 +0100

    Only showing active products now.

--- admin.py
+++ admin.py
@@ -6,7 +6,7 @@ from basic_webshop.models import *
 class ProductAdmin(admin.ModelAdmin):
     """ Model admin for products. """
     
-    fields = ('name', 'slug', 'category', 'price', 'description')
+    fields = ('name', 'slug', 'active', 'category', 'price', 'description')
     prepopulated_fields = {"slug": ("name",)}
 
     
