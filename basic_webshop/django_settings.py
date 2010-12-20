6969025e56c5895e0c23555c01bde03baa500bbb
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-12-20 15:53:59 +0100

    Added django_settings file.

---
+++ basic_webshop/django_settings.py
@@ -0,0 +1,15 @@
+""" 
+These settings should be imported in `settings.py` by adding::
+
+    from basic_webshop.django_settings import *
+
+"""
+
+WEBSHOP_CUSTOMER_MODEL = 'basic_webshop.Customer'
+WEBSHOP_PRODUCT_MODEL = 'basic_webshop.Product'
+WEBSHOP_CART_MODEL = 'basic_webshop.Cart'
+WEBSHOP_CARTITEM_MODEL = 'basic_webshop.CartItem'
+WEBSHOP_ORDER_MODEL = 'basic_webshop.Order'
+WEBSHOP_ORDERITEM_MODEL = 'basic_webshop.OrderItem'
+WEBSHOP_CATEGORY_MODEL = 'basic_webshop.Category'
+
