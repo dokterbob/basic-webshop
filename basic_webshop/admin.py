cb960dec0ef10a5b28790379b3ab0df96e9ed4b2
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-01-17 22:40:47 +0100

    Using a quirky workaround, we can select only variants relevant for our current product.
    TODO: refactor and move some of this stuff to
    django-webshop instead.

--- basic_webshop/admin.py
+++ basic_webshop/admin.py
@@ -1,5 +1,5 @@
 import logging
-
+logger = logging.getLogger(__name__)
 
 from django.contrib import admin
 
@@ -10,40 +10,62 @@ from webshop.extensions.images.admin import ProductImageInline, ImagesProductMix
 
 from multilingual_model.admin import TranslationInline
 
+from basic_webshop.forms import VariationInlineForm
 
 class VariationInlineMixin(object):
-    """ Make sure we can only select variations that relate to the current
+    #form = VariationInlineForm
+    #formset = None
+    # http://stackoverflow.com/questions/1824267/limit-foreign-key-choices-in-select-in-an-inline-form-in-admin
+    # 
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        """
+        Make sure we can only select variations that relate to the current
         product. 
         
         This should be part of the django-webshop variations extension.
-    """
-    # http://stackoverflow.com/questions/1824267/limit-foreign-key-choices-in-select-in-an-inline-form-in-admin
-    # 
-    # def formfield_for_foreignkey(self, *args, **kwargs):
-    #     logger.debug('kaas')
-    #     return super(VariationInlineMixin).formfield_for_foreignkey(self, *args, **kwargs)
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-    #     # # TODO: Somehow figure out the current product
-    #     
-    #     logger.debug('Dingding')
-    #     
-    #     # if db_field.name == "variation":
-    #     #     # If no instance is given, it makes sense to not be able to
-    #     #     # select any of its variations.
-    #     #     if self.instance:
-    #     #         qs = Variation.objects.filter(product=self.instance)
-    #     #     else:
-    #     #         qs = Variation.objects.none()
-    #     #     
-    #     #     kwargs["queryset"] = qs
-    #     
-    #     return super(VariationInlineMixin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        TODO: Unittest this mother...
+        
+        """
+        field = super(VariationInlineMixin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        if db_field.name == "variation":
+            if request._obj_ is not None:
+                # logger.error('monkeybrains')
+                
+                field.queryset = field.queryset.filter(product__exact = request._obj_)  
+            else:
+                #logger.error('horsecrap')
+                field.queryset = field.queryset.none()
+            # If no instance is given, it makes sense to not be able to
+            # select any of its variations.
+            
+            # import pdb; pdb.set_trace()
+            # logging.error('poop')
+            # logging.error(formfield.queryset)
+            # instance = getattr(self, 'instance', None)
+            # 
+            # logging.error(instance)
+            # product = request._obj
+            # 
+            # qs = formfield.queryset
+            # 
+            # if instance:
+            #     qs  = qs.filter(product=self.instance)
+            # else:
+            #     qs  = qs.none()
+                
+                
+            # 
+            # qs = Variation.objects.all()
+            # kwargs["queryset"] = qs
+            # logging.debug(kwargs)
+
+        return field
 
 
-class VariationPriceInline(PriceInline, VariationInlineMixin):
+class VariationPriceInline(VariationInlineMixin, PriceInline):
     pass
 
-class ProductVariationTranslationInline(admin.TabularInline, VariationInlineMixin):
+class ProductVariationTranslationInline(VariationInlineMixin, admin.TabularInline):
     # TODO: Limit the selection of parents to those associated with the 
     # current product.
     model = ProductVariationTranslation
@@ -73,6 +95,11 @@ class ProductAdmin(admin.ModelAdmin, ImagesProductMixin):
         return u'<a href="%d/">%s</a>' % \
             (obj.pk, obj.name)
     name.allow_tags = True
+
+    def get_form(self, request, obj=None, **kwargs):
+        # just save obj reference for future processing in Inline
+        request._obj_ = obj
+        return super(ProductAdmin, self).get_form(request, obj, **kwargs)
 
 admin.site.register(Product, ProductAdmin)
 
