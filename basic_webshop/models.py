a969e5730be2922e3aeb8b95ad89536f431f2519
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-02-12 13:51:34 +0100

    Cleanup: base models belong in basemodels.py from now.

--- basic_webshop/models.py
+++ basic_webshop/models.py
@@ -23,6 +23,12 @@ from webshop.extensions.stock.advanced.models import StockedCartItemMixin, \
                                                    StockedItemMixin
 from webshop.extensions.related.models import RelatedProductsMixin
 from webshop.extensions.brands.models import BrandBase, BrandedProductMixin
+from webshop.extensions.discounts.models import DiscountBase, \
+                                                ManyProductDiscountMixin, \
+                                                DateRangeDiscountMixin, \
+                                                ManyCategoryDiscountMixin, \
+                                                LimitedUseDiscountMixin, \
+                                                CouponDiscountMixin
 
 from multilingual_model.models import MultilingualModel, \
                                       MultilingualTranslation
@@ -31,107 +37,12 @@ from tinymce import models as tinymce_models
 
 from sorl.thumbnail import ImageField
 
+from basic_webshop.basemodels import *
 
-### All the stuff below should end up in django-webshop, eventually
 
 class Customer(UserCustomerBase):
     """ Basic webshop customer. """
     pass
-
-
-class UniqueSlugItemBase(models.Model):
-    """ Base class for items which require a slug field which should be unique. """
-
-    class Meta:
-        abstract = True
-
-    slug = models.SlugField(unique=True, help_text=_('Short name for an item, \
-            used for constructing its web addres. A slug should be unique and may only \
-            contain letters, numbers and \'-\'.'), blank=True)
-
-
-from django.template.defaultfilters import slugify
-class AutoSlugMixin(object):
-    """ Automatically set slug to slugified version of the name if left empty. """
-
-    def slugify(self, name):
-        return slugify(name)
-
-    def generate_slug(self):
-        return self.slugify(self.name)
-
-    def update_slug(self, commit=True):
-        if not self.slug and self.name:
-            self.slug = self.generate_slug()
-            
-            if commit:
-                self.save()
-
-
-class AutoUniqueSlugMixin(AutoSlugMixin):
-    """ Make sure that the generated slug is unique. """
-
-    def is_unique_slug(self, slug):
-        return not self.__class__.objects.filter(slug=slug).exists()
-
-    def generate_slug(self):
-        original_slug = super(AutoUniqueSlugMixin, self).generate_slug()
-        slug = original_slug
-
-        iteration = 1
-        while not self.is_unique_slug(slug):
-            slug = "%s-%d" % (original_slug, iteration)
-            iteration += 1
-
-        return slug
-
-
-class NonUniqueSlugItemBase(models.Model):
-    """ Base class for items which require a slug field which should not be unique. """
-
-    class Meta:
-        abstract = True
-
-    slug = models.SlugField(unique=False, help_text=_('Short name for an item, \
-            used for constructing its web addres. A slug may only \
-            contain letters, numbers and \'-\'.'), blank=True)
-
-
-class FeaturedProductMixin(models.Model):
-    """
-    Mixin for products which have a boolean featured property and an
-    `is_featured` manager, filtering the items from the `in_shop` manager
-    so that only featured items are returned.
-
-    .. todo::
-        Write the `is_featured` manager - and test it.
-
-    """
-
-    class Meta:
-        abstract = True
-
-    featured = models.BooleanField(_('featured'), default=False,
-                               help_text=_('Whether this product will be \
-                               shown on the shop\'s frontpage.'))
-
-
-### All the stuff above should end up in django-webshop, eventually
-
-
-class NamedItemTranslationMixin(object):
-    """
-    Mixin for translated items with a name.
-    This makes sure that abstract base classes that rely on __unicode__
-    will work with the translated __unicode__ name.
-
-    Usage::
-        class Banana(AbstractBaseClass, NamedItemTranslationMixin):
-            ...
-
-    """
-    def __unicode__(self):
-        return self.unicode_wrapper('name')
 
 
 class Brand(AutoUniqueSlugMixin, NamedItemTranslationMixin, MultilingualModel, \
@@ -338,7 +249,6 @@ class Category(MultilingualModel, NonUniqueSlugItemBase, AutoUniqueSlugMixin, Ac
             {'slug': self.slug}
 
 
-
 class CategoryTranslation(NamedItemBase, MultilingualTranslation):
     class Meta(MultilingualTranslation.Meta, NamedItemBase.Meta):
         unique_together = (('language_code', 'parent',), )
@@ -349,13 +259,6 @@ class CategoryTranslation(NamedItemBase, MultilingualTranslation):
         super(CategoryTranslation, self).save()
 
         self.parent.update_slug()
-
-from webshop.extensions.discounts.models import DiscountBase, \
-                                                ManyProductDiscountMixin, \
-                                                DateRangeDiscountMixin, \
-                                                ManyCategoryDiscountMixin, \
-                                                LimitedUseDiscountMixin, \
-                                                CouponDiscountMixin
 
 
 class Discount(NamedItemBase, ManyCategoryDiscountMixin, CouponDiscountMixin, \
