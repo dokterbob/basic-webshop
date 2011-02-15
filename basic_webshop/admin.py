3d75e30987b79afeea485d0d8950e719e8e278fc
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-02-15 21:04:47 +0100

    Got rid of redundant whitespace and making use of the 'new' LimitedInlineAdmin API now

--- basic_webshop/admin.py
+++ basic_webshop/admin.py
@@ -28,50 +28,30 @@ class BrandAdmin(AdminInlineImageMixin, admin.ModelAdmin):
 
 admin.site.register(Brand, BrandAdmin)
 
-"""
-TODO/bug:
-    Overriode ProductVariationInline so that we can only select images
-    associated with the current product.
-""" 
-
 
 class ImageProductVariationInline(LimitedAdminInlineMixin, ProductVariationInline):
-    def get_formset(self, request, obj=None, **kwargs):
+    def get_filters(self, obj):
         """
         Make sure we can only select variations that relate to the current
         item.
         """
-        formset = super(ImageProductVariationInline, self).get_formset(request, obj=None, **kwargs)
-        
-        if obj:
-            self.limit_inline_choices(formset, 'image', product=obj)
-        else:
-            self.limit_inline_choices(formset, 'image', empty=True)
-
-        return formset
+        return (('image', dict(product=obj)),)
 
 
 class ProductVariationTranslationInline(LimitedAdminInlineMixin, admin.TabularInline):
-    """ 
+    """
     TODO/bug:
-        VariationInlineMixin should limit the variations we can select to those 
+        VariationInlineMixin should limit the variations we can select to those
         associated with the current product, but this doesn't work.
     """
     model = ProductVariationTranslation
 
-    def get_formset(self, request, obj=None, **kwargs):
+    def get_filters(self, obj):
         """
         Make sure we can only select variations that relate to the current
         item.
         """
-        formset = super(ProductVariationTranslationInline, self).get_formset(request, obj=None, **kwargs)
-        
-        if obj:
-            self.limit_inline_choices(formset, 'parent', product=obj)
-        else:
-            self.limit_inline_choices(formset, 'parent', empty=True)
-
-        return formset
+        return (('parent', dict(product=obj)),)
 
 
 class ProductTranslationInline(TranslationInline):
@@ -147,7 +127,7 @@ class ProductAdmin(InlineButtonsAdminMixin, ImagesProductAdminMixin, \
                ProductVariationTranslationInline,
               )
     filter_horizontal = ('categories', 'related')
-    
+
     list_display = ('display_name', 'default_image', 'admin_categories', \
                     'sort_order', 'active', )
     # list_display_links = ('name', )
@@ -163,22 +143,22 @@ class ProductAdmin(InlineButtonsAdminMixin, ImagesProductAdminMixin, \
         """ TODO: Move this over to django-webshop's extension. """
         categories = obj.categories.all()
         categories_count = categories.count()
-        
+
         def category_link(obj):
             return u'<a href="../category/%d/">%s</a>' % \
                 (obj.pk, obj)
-            
+
         if categories_count == 0:
             return _('None')
         else:
             category_list = category_link(categories[0])
-            
+
             for category in categories[1:self.max_categories_display]:
                 category_list += u', %s' % category_link(category)
-            
+
             if categories_count > self.max_categories_display:
                 category_list += u', ...'
-            
+
             return category_list
     admin_categories.allow_tags = True
     admin_categories.short_description = _('categories')
@@ -186,20 +166,20 @@ class ProductAdmin(InlineButtonsAdminMixin, ImagesProductAdminMixin, \
     def get_media_link_list(self, request, object_id):
         """ Get the media available for linking to. """
         obj = self._getobj(request, object_id)
-        
+
         product_media = obj.productmedia_set.all()
         link_list = []
         for media in product_media:
             link_list.append((media.name, media.mediafile.url))
-                
+
         return render_to_link_list(link_list)
 
     def get_urls(self):
         urls = super(ProductAdmin, self).get_urls()
-        
+
         my_urls = patterns('',
-            url(r'^(.+)/media_link_list.js$', 
-                self._wrap(self.get_media_link_list), 
+            url(r'^(.+)/media_link_list.js$',
+                self._wrap(self.get_media_link_list),
                 name=self._view_name('media_link_list')),
         )
 
@@ -217,20 +197,12 @@ class CategoryFeaturedInline(LimitedAdminInlineMixin, admin.TabularInline):
     extra =  1
     fields = ('featured_order', 'product')
 
-    def get_formset(self, request, obj=None, **kwargs):
+    def get_filters(self, obj):
         """
         Make sure we can only select variations that relate to the current
         item.
         """
-        formset = \
-            super(CategoryFeaturedInline, self).get_formset(request, obj=None, **kwargs)
-
-        if obj:
-            self.limit_inline_choices(formset, 'product', categories=obj)
-        else:
-            self.limit_inline_choices(formset, 'product', empty=True)
-
-        return formset
+        return (('product', dict(categories=obj)),)
 
 
 from mptt.admin import MPTTModelAdmin
@@ -251,13 +223,13 @@ class CategoryAdmin(MPTTModelAdmin):
     def admin_name(self, obj):
         return obj.name
     admin_name.short_description = _('name')
-    
+
     max_products_display = 2
     def admin_products(self, obj):
         products = obj.product_set.all()
         products_count = products.count()
         products_inactive_count = products.filter(active=False).count()
-        
+
         if not products_count:
             value = _('No products')
         elif products_count == 1:
@@ -266,19 +238,19 @@ class CategoryAdmin(MPTTModelAdmin):
         else:
             value = u'<a href="../product/?categories__id__exact=%d">%d products</a>' \
                 % (obj.pk, products_count)
-        
+
         if products_inactive_count:
             value += u' <a href="">(%d inactive)</a>' % \
                 (obj.pk, products_inactive_count)
-        
+
         value += u'&nbsp;|&nbsp;<a href="../product/add/?categories=%d">Add</a>' \
             % (obj.pk, )
-        
+
         return value
-        
+
     admin_products.allow_tags = True
     admin_products.short_description = _('products')
-   
+
 admin.site.register(Category, CategoryAdmin)
 
 
@@ -299,13 +271,13 @@ class DiscountAdmin(admin.ModelAdmin):
             product_list = _('None')
         else:
             product_list = unicode(products[0])
-            
+
             for product in products[1:self.max_products_display]:
                 product_list += u', %s' % product
-            
+
             if products_count > self.max_products_display:
                 product_list += u', ...'
-        
+
         return u'<a href="%d/">%s</a>' % (obj.pk, product_list)
     admin_products.allow_tags = True
     admin_products.short_description = _('products')
