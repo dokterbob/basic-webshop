2100ad346fb0b94537066c789a09f1b9a49b5357
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-03-16 17:33:16 +0100

    Configurable monetary locale

--- basic_webshop/django_settings.py
+++ basic_webshop/django_settings.py
@@ -36,5 +36,7 @@ WEBSHOP_SHIPPING_ADDRESS_MODEL = 'basic_webshop.Address'
 WEBSHOP_SHIPPING_METHOD_MODEL = 'basic_webshop.ShippingMethod'
 
 WEBSHOP_CURRENCY_PRICE_FIELD = 'webshop.extensions.currency.simple.fields.PriceField'
+WEBSHOP_CURRENCY_LOCALE = 'nl_NL.ISO8859-1'
+
 
 
