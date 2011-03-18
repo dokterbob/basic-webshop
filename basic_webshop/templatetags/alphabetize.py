2f3eeedf6c6ecfc0937cfadcbb8c58b1a808bf3e
Alexander Schrijver <alexander.schrijver@gmail.com>
2011-03-18 12:48:50 +0100

    Add a templatetag to deal with an alphabetized list.

---
+++ basic_webshop/templatetags/alphabetize.py
@@ -0,0 +1,23 @@
+from django import template
+import string
+
+register = template.Library()
+
+# TODO: Deal with brands which don't start with A-Z.
+def alphabetize(object_list):
+    r_list = []
+    for char in string.ascii_uppercase:
+        r_objects = []
+        for object in object_list:
+            if str(object)[0].upper() == char:
+                r_objects.append(object)
+
+        if r_objects:
+            r_list.append((char, r_objects))
+            
+    return {
+        'list': r_list
+    }
+# Register the custom tag as an inclusion tag with takes_context=True.
+register.inclusion_tag('basic_webshop/alphabetize.html')(alphabetize)
+
