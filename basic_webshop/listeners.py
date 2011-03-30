e7c6c9f70907aa55c1bb663727151e3c90caf32b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-03-30 19:47:41 +0200

    Eerste aanzet email listeners

--- basic_webshop/listeners.py
+++ basic_webshop/listeners.py
@@ -4,6 +4,8 @@ logger = logging.getLogger(__name__)
 from django.utils.decorators import classonlymethod
 from django.utils.functional import update_wrapper
 
+from django.core.exceptions import ImproperlyConfigured
+
 
 class Listener(object):
     """
@@ -57,6 +59,83 @@ class Listener(object):
 
     def dispatch(self, sender, **kwargs):
         raise NotImplementedError('Sublcasses should implement this!')
+
+
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+class EmailingListener(Listener):
+    """ Listener which sends out emails. """
+
+    body_template_name = None
+    subject_template_name = None
+
+    def get_subject_template_names(self):
+        """
+        Returns a list of template names to be used for the request. Must return
+        a list. May not be called if render_to_response is overridden.
+        """
+        if self.subject_template_name is None:
+            raise ImproperlyConfigured(
+                "TemplateResponseMixin requires either a definition of "
+                "'template_name' or an implementation of 'get_template_names()'")
+        else:
+            return [self.subject_template_name]
+
+    def get_body_template_names(self):
+        """
+        Returns a list of template names to be used for the request. Must return
+        a list. May not be called if render_to_response is overridden.
+        """
+        if self.body_template_name is None:
+            raise ImproperlyConfigured(
+                "TemplateResponseMixin requires either a definition of "
+                "'template_name' or an implementation of 'get_template_names()'")
+        else:
+            return [self.body_template_name]
+
+    def get_context_data(self):
+        """
+        Context for the message template rendered. Defaults to sender and
+        kwargs.
+        """
+        context = {'sender': self.sender}
+
+        context.update(self.kwargs)
+
+        return context
+
+    def get_recipients(self):
+        """ Get recipients for the message. """
+        raise NotImplementedError
+
+    def get_sender(self):
+        """
+        Sender of the message, defaults to `None` which imples
+        `DEFAULT_FROM_EMAIL`.
+        """
+        return None
+
+    def create_message(self, context):
+        """ Create an email message. """
+        subject = render_to_string(self.get_subject_template_names(), context)
+        body = render_to_string(self.get_body_template_names(), context)
+        recipients = self.get_recipients()
+        sender = self.get_sender()
+
+        email = EmailMessage(subject, body, sender, recipients)
+
+        return email
+
+    def handler(self, sender, **kwargs):
+        """ Store sender and kwargs attributes on self. """
+        self.sender = sender
+        self.kwargs = kwargs
+
+        context = self.get_context_data(**kwargs)
+
+        message = self.create_message(context)
+
+        message.send()
 
 
 class OrderPaidListener(Listener):
@@ -121,6 +200,8 @@ class StatusChangeListener(Listener):
 
 
 class OrderPaidConfirm(StatusChangeListener):
+    """ Confirm paid orders. """
+
     old_state = order_states.ORDER_STATE_PENDING
     state = order_states.ORDER_STATE_PAID
 
@@ -134,3 +215,49 @@ class OrderPaidConfirm(StatusChangeListener):
         logger.debug(u'Confirming paid order %s', order)
         order.confirm()
 
+
+class OrderStateChangeEmail(EmailingListener, StatusChangeListener):
+    """
+    Send emails upon order state change.
+
+    The following variables will be available in the email and subject
+    templates::
+
+        `order`
+        `customer`
+        `address`
+        `state_change`
+
+
+    Use this class as follows::
+
+        class OrderPaidEmail(OrderStateChangeEmail):
+            state = order_states.ORDER_STATE_PAID
+            body_template_name = 'basic_webshop/emails/order_paid_body.txt'
+            subject_template_name = 'basic_webshop/emails/order_paid_subject.txt'
+
+    """
+
+    def get_context_data(self):
+        context = super(OrderPaidEmail, self).get_context_data()
+
+        context['order'] = self.sender
+        context['customer'] = self.kwargs['customer']
+        context['address'] = self.kwargs['address']
+        context['state_change'] = self.kwargs['state_change']
+
+        return context
+
+    def get_recipients(self):
+        order = self.sender
+        assert order.customer
+        assert order.customer.email
+
+        return (order.customer.email, )
+
+class OrderPaidEmail(OrderStateChangeEmail):
+    """ Send email when order paid. """
+
+    state = order_states.ORDER_STATE_PAID
+    body_template_name = 'basic_webshop/emails/order_paid_body.txt'
+    subject_template_name = 'basic_webshop/emails/order_paid_subject.txt'
