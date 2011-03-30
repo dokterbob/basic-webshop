b3a6b2a8905715414d99703c64d84df770006fd4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-03-30 22:50:32 +0200

    Updated order states. Also, added extra decimals so we can add more in between later.

--- basic_webshop/order_states.py
+++ basic_webshop/order_states.py
@@ -2,18 +2,20 @@ from django.utils.translation import ugettext_lazy as _
 
 
 ORDER_STATE_NEW = 0
-ORDER_STATE_PENDING = 1
-ORDER_STATE_PAID = 2
-ORDER_STATE_CANCELED = 3
-ORDER_STATE_REJECT = 4
-ORDER_STATE_SHIPPED = 5
+ORDER_STATE_PENDING = 10
+ORDER_STATE_PAID = 20
+ORDER_STATE_CANCELED = 30
+ORDER_STATE_REJECT = 40
+ORDER_STATE_PROCESSED = 50
+ORDER_STATE_SHIPPED = 60
 
 ORDER_STATES = \
     (
         (ORDER_STATE_NEW, _('New')),
-        (ORDER_STATE_PENDING, _('Pending')),
+        (ORDER_STATE_PENDING, _('Payment pending')),
         (ORDER_STATE_PAID, _('Paid')),
         (ORDER_STATE_CANCELED, _('Canceled')),
         (ORDER_STATE_REJECT, _('Rejected')),
+        (ORDER_STATE_PROCESSED, _('Being processed')),
         (ORDER_STATE_SHIPPED, _('Shipped')),
     )
