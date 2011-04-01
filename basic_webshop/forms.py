23b186fa15e4bb19a71490fb22dc31d377ec552f
Alexander Schrijver <alexander.schrijver@gmail.com>
2011-04-01 15:06:03 +0200

    Change the CartAdd textfield into an optionfield.

--- basic_webshop/forms.py
+++ basic_webshop/forms.py
@@ -68,7 +68,30 @@ class CartAddForm(forms.Form):
 
         return quantity
 
-    quantity = forms.IntegerField(min_value=1, initial=1)
+    # TODO: Prolly nicer to generate this in a certain way.
+    QUANTITY_CHOICES =  (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+        (9, 9),
+        (10, 10),
+        (11, 11),
+        (12, 12),
+        (13, 13),
+        (14, 14),
+        (15, 15),
+        (16, 16),
+        (17, 17),
+        (18, 18),
+        (19, 19),
+        (20, 20),
+    )
+    quantity = forms.IntegerField(widget=forms.Select(choices=QUANTITY_CHOICES), min_value=1, initial=1)
 
 
 class AddressUpdateForm(forms.ModelForm):
