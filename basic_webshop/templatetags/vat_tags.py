f0328e07d837467074b703634dd7a47573af05f0
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-04-18 10:20:21 +0200

    Added template tag for VAT-exclusive amount.

--- basic_webshop/templatetags/vat_tags.py
+++ basic_webshop/templatetags/vat_tags.py
@@ -7,18 +7,24 @@ register = template.Library()
 
 VAT_PERCENTAGE = getattr(settings, 'WEBSHOP_VAT_PERCENTAGE', 19)
 
-vat_factor = VAT_PERCENTAGE/100.0
+vat_factor = Decimal(VAT_PERCENTAGE/100.0)
 
 
 @register.filter
 def vat_amount(value):
     """ Total amount of VAT for amount. """
-    amount = float(value)
+    amount = Decimal(value)
     return amount*vat_factor
 
 @register.filter
 def vat_inclusive(value):
     """ Amount inlcuding VAT. """
-    amount = float(value)
+    amount = Decimal(value)
     return value*(1+vat_factor)
-    
+
+@register.filter
+def vat_exclusive(value):
+    """ Amount inlcuding VAT. """
+    amount = Decimal(value)
+    return value*(1-vat_factor)
+
